f222c23925549712a7fd673dbbaccb5c98bea0d4
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2021-03-12 01:39:54 -0800

    Update clipboard_dataframe.R
    Fixed problems with the initial commit, and added automatic type parsing with regular expressions.
    Now (unless you set parse_types to FALSE), regular expressions will detect if a column has all integers or all floats and "NAN"-type strings, and coerce columns that fit the bill to the appropriate data type, leaving other columns as character.
    Also added a toy data set used to test convert_types_list, the type coercion function.

--- clipboard_dataframe.R
+++ clipboard_dataframe.R
@@ -1,27 +1,34 @@
 library(stringr)
 library(stringi)
+library(dplyr)
+
 
 string_rows_dataframe = function(strvec,
                                  sep='\\s+',
                                  head_row=NULL,
                                  ignore_rows=NULL,
-                                 ignore_cols=NULL) {
-  strvec = mapply(str_trim,strvec,'right')   #remove hanging whitespace from ends of rows
+                                 ignore_cols=NULL,
+                                 parse_types = TRUE) {
+  strvec = mapply(str_trim,strvec,'right') 
+  #remove hanging whitespace from ends of rows
   if (! is.null(head_row)) {
     if (! (length(head_row)==1 & is.numeric(head_row))) { 
-      #head_row is not null and not a row number
-      #that means that head_row is a list of column names, i.e., the header
+      #head_row is a row number
       header = head_row
     } else {
-      #head_row is a row number; use the head_row^th row of the input as a header
+      #head
       header = na.exclude(str_split(strvec[[head_row]],sep))[[1]]
     }
-    if (! is.null(ignore_rows)) { #cull the ignored rows and/or header from input
+    if (! is.null(ignore_rows)) {
       if (is.numeric(head_row)) {
-        strvec = strvec[-c(head_row,ignore_rows)] 
+        strvec = strvec[-c(head_row,ignore_rows)]
+      }  else {
+        strvec = strvec[-ignore_rows]
       }
     } else {
-      strvec = strvec[-head_row]
+      if (is.numeric(head_row)) {
+        strvec = strvec[-head_row]
+      }
     }
   } else {
     header <- NULL
@@ -29,11 +36,17 @@ string_rows_dataframe = function(strvec,
       strvec = strvec[-ignore_rows]
     }
   }
-  strvec_split <- na.exclude(mapply(str_split,strvec,sep))  #split all rows with designated separator
+  
+  strvec_split <- na.exclude(mapply(str_split,strvec,sep))
   transposed_frame = t(data.frame(strvec_split))
   out_dframe = as.data.frame(transposed_frame,
                              row.names=FALSE,
                              fix.empty.names = TRUE)
+  
+  if (parse_types==TRUE) { #Use the regular expressions below to automatically parse column types and convert them
+    out_dframe <- as.data.frame(lapply(out_dframe,convert_types_list))
+  }
+  
   if (! is.null(ignore_cols)) {
     vvec = sapply(mapply(str_glue,ignore_cols,c("V")),
                   stri_reverse)
@@ -53,16 +66,50 @@ string_rows_dataframe = function(strvec,
 clipboard_dataframe = function(sep="\\s+",
                                head_row=NULL,
                                ignore_rows=NULL,
-                               ignore_cols=NULL) {
+                               ignore_cols=NULL,
+                               parse_types = TRUE) {
   strvec = readClipboard()
-  string_rows_dataframe(strvec,sep,head_row,ignore_rows,ignore_cols)
+  string_rows_dataframe(strvec,sep,head_row,ignore_rows,ignore_cols,parse_types)
 }
 
 string_dataframe = function(string,
                             sep = '\\s+',
                             head_row=NULL,
                             ignore_rows=NULL,
-                            ignore_cols=NULL) {
-  strvec = str_split(string,'\r?\n')[[1]] #splits a string by the newline character
-  string_rows_dataframe(strvec,sep,head_row,ignore_rows,ignore_cols)
+                            ignore_cols=NULL,
+                            parse_types = TRUE) {
+  strvec = str_split(string,'\r?\n')[[1]]
+  string_rows_dataframe(strvec,sep,head_row,ignore_rows,ignore_cols,parse_types)
 }
+
+numregex = '^-?\\d*\\.?\\d+$'
+intregex = '^-?\\d+$'
+nanregex= '(?i)(^na[n]?$|^$)' 
+#(?i) at the beginning makes entire regex case-insensitive
+
+convert_types_list <- function(list_) {
+  len_ = length(list_)
+  nancount = len_ - sum(is.na(str_match(list_,nanregex))[,1])
+  floatcount = len_ - sum(is.na(str_match(list_,numregex))[,1])
+  intcount = len_ - sum(is.na(str_match(list_,intregex))[,1])
+  if (intcount == len_) { #list_ is all integers, so make it ints
+    as.integer(list_)
+  } else if (floatcount + nancount == len_) {
+    as.numeric(list_) #if list_ is all floats and nan's, make it numeric
+  } else {
+    list_ #if all else fails, leave it as characters
+  }
+}
+
+assorted_strings = c('a','1b','12.5','','nan','NaN','-7.8c','-1.2','-3','b',
+             'na','NA','2','NaCl','naturally')
+
+all_ints = c('39', '64', '87', '36', '22', '1', '60', '75', 
+             '12', '45', '97', '5', '30', '38', '18')
+
+nums_and_nans = c('1','','nan','2','13.4','-400.23',
+                  '-0.9758769273310667', '-1.155026681824496', 
+                  '-0.17578621901546307', '-0.2326359733469704', '-0.5770294020936887', 
+                  '-0.08014278356830644', '-1.352910994914262', 
+                  '-0.9283167810811379', '2.270282854269814')
+convert_types_list_test_df = data.frame(stuff=stuff,ints=all_ints,floats=nums_and_nans)
